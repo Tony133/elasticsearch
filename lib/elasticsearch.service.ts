78ba33535b29f25e94e9a2741ffcd57c16498d9c
Tony133 <a.tripodi133@gmail.com>
2023-06-17 16:53:53 +0200

    feat(): marked api public

--- lib/elasticsearch.service.ts
+++ lib/elasticsearch.service.ts
@@ -2,6 +2,9 @@ import { Client, ClientOptions } from '@elastic/elasticsearch';
 import { Inject, Injectable, Optional } from '@nestjs/common';
 import { ELASTICSEARCH_MODULE_OPTIONS } from './elasticsearch.constants';
 
+/**
+ * @publicApi
+ */
 @Injectable()
 export class ElasticsearchService extends Client {
   constructor(
--- lib/interfaces/elasticsearch-module-options.interface.ts
+++ lib/interfaces/elasticsearch-module-options.interface.ts
@@ -1,20 +1,41 @@
 import { ClientOptions } from '@elastic/elasticsearch';
 import { ModuleMetadata, Type } from '@nestjs/common/interfaces';
 
+/**
+ * @publicApi
+ */
 export type ElasticsearchModuleOptions = ClientOptions;
 
+/**
+ * @publicApi
+ */
 export interface ElasticsearchOptionsFactory {
   createElasticsearchOptions():
     | Promise<ElasticsearchModuleOptions>
     | ElasticsearchModuleOptions;
 }
 
+/**
+ * @publicApi
+ */
 export interface ElasticsearchModuleAsyncOptions
   extends Pick<ModuleMetadata, 'imports'> {
+  /**
+   * The `useExisting` syntax allows you to create aliases for existing providers.
+   */
   useExisting?: Type<ElasticsearchOptionsFactory>;
+  /**
+   * The `useClass` syntax allows you to dynamically determine a class that a token should resolve to.
+   */
   useClass?: Type<ElasticsearchOptionsFactory>;
+  /**
+   * The `useFactory` syntax allows for creating providers dynamically.
+   */
   useFactory?: (
     ...args: any[]
   ) => Promise<ElasticsearchModuleOptions> | ElasticsearchModuleOptions;
+  /**
+   * Optional list of providers to be injected into the context of the Factory function.
+   */
   inject?: any[];
 }
